edc54f0536098dc535592bd8dbde665ba93c5f3d
useryamini <96192360+useryamini@users.noreply.github.com>
2021-12-26 22:24:53 +0530

    Add files via upload

---
+++ MenuItemListing/MenuItemListing/Controllers/MenuItemController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MenuItemListing.Models;
+
+namespace MenuItemListing.Controllers
+{
+    public class MenuItemController : Controller
+    {
+        List<MenuItem> menu;
+        
+        [HttpGet("id")]
+        public IActionResult GetDetails(int id)
+        {
+            menu = new List<MenuItem>() {
+            new MenuItem()
+            {
+                Id = 1001,
+                Name = "ChickenCurry",
+                freeDelivery = true,
+                Price = 300,
+                DateOfLaunch = Convert.ToDateTime("2021-12-26"),
+                Active = true
+
+
+            },
+
+            new MenuItem()
+            {
+                Id = 1002,
+                Name = "VegMixedCurry",
+                freeDelivery = false,
+                Price = 350,
+                DateOfLaunch = Convert.ToDateTime("2021-12-26"),
+                Active = true
+
+            }
+            };
+
+           var MenuItems = menu.FirstOrDefault(x => x.Id == id);
+            return Ok(MenuItems);
+
+        }
+     
+            
+     }
+ }
+
---
+++ MenuItemListing/MenuItemListing/Models/MenuItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MenuItemListing.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public  int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        
+
+        [Display(Name="free Delivery")]
+
+        public bool freeDelivery { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfLaunch { get; set; }
+
+         public  bool Active { get; set; }
+    }
+}
